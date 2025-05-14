2fd203ba5f738bb901b5f7a341865d00faa4f9fd
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-05-14 13:07:57 +0530

    grades pages

---
+++ get_student_department_id.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: application/json');
+
+include('db.php'); // Include your database connection file
+$college_name =$_GET['college_name']
+$dept_name =$_GET['dept_name']
+
+$conn->close();
+?>
